a98c14ade661b4185c3b201044a3713bb520a4ad
chunquedong <chunquedong@localhost>
2011-07-31 20:26:13 +0800

    Enhance ArrayBuffer

--- fogl/fan/ArrayBuffer.fan
+++ fogl/fan/ArrayBuffer.fan
@@ -7,15 +7,72 @@
 //
 
 **
+** primeval number type
+**
+@Js
+enum class NumType
+{
+  tInt(4), tShort(2), tLong(8), tDouble(8), tFloat(4), tChar(2), tByte(1)
+
+  private new make(Int size) { this.size = size }
+  const Int size;
+}
+
+**
 ** Wrapping a native array buffer.
 **
 @Js
 class ArrayBuffer
 {
-  native static ArrayBuffer makeFloat(Float[] data)
+  native Int size
+  native Int pos
+  native Endian endian
+  native NumType type
 
-  native static ArrayBuffer makeInt(Int[] data)
+  native Int capacity()
+  native Bool isDirect()
 
-  native static ArrayBuffer makeShort(Int[] data)
+  native static ArrayBuffer allocateDirect(Int size, NumType type := NumType.tByte)
+  native static ArrayBuffer allocate(Int size, NumType type := NumType.tByte)
+
+  native This flip()
+  native Int remaining()
+
+  native This readTo(Num[] dst, Int offset := 0, Int length := dst.size)
+  native This writeFrom(Num[] src, Int offset := 0, Int length := src.size)
+
+  native Int getInt(Int index)
+  native This setInt(Int index, Int v)
+  native Int getFloat(Int index)
+  native This setFloat(Int index, Int v)
+
+  native This putFloat(Float[] data)
+  native This putInt(Int[] data)
+  native This putShort(Int[] data)
+
+  internal new make() {}
+
+  static ArrayBuffer makeFloat(Float[] data)
+  {
+    buf := ArrayBuffer.allocateDirect(data.size, NumType.tFloat)
+    buf.putFloat(data)
+    buf.flip
+    return buf
+  }
+
+  static ArrayBuffer makeInt(Int[] data)
+  {
+    buf := ArrayBuffer.allocateDirect(data.size, NumType.tInt)
+    buf.putInt(data)
+    buf.flip
+    return buf
+  }
+
+  static ArrayBuffer makeShort(Int[] data)
+  {
+    buf := ArrayBuffer.allocateDirect(data.size, NumType.tShort)
+    buf.putShort(data)
+    buf.flip
+    return buf
+  }
 }
-
--- fogl/java/ArrayBufferPeer.java
+++ fogl/java/ArrayBufferPeer.java
@@ -17,55 +17,207 @@ import org.lwjgl.BufferUtils;
 
 class ArrayBufferPeer
 {
-  private java.nio.Buffer data;
+  private java.nio.Buffer bufferView;
+  private ByteBuffer rawData;
 
   public static ArrayBufferPeer make(ArrayBuffer self) { return new ArrayBufferPeer(); }
 
-  public static ArrayBuffer makeFloat(List list)
+//////////////////////////////////////////////////////////////////////////
+// fields
+//////////////////////////////////////////////////////////////////////////
+
+  public long size(ArrayBuffer self) { return getValue().limit(); }
+  public void size(ArrayBuffer self, long v) { getValue().limit((int)v); }
+
+  public long pos(ArrayBuffer self) { return getValue().position(); }
+  public void pos(ArrayBuffer self, long v) { getValue().position((int)v); }
+
+  public Endian endian(ArrayBuffer self)
   {
-    ArrayBuffer buffer = ArrayBuffer.make();
-    makeFloat$(buffer, list);
-    return buffer;
+    return (rawData.order() == ByteOrder.BIG_ENDIAN) ? Endian.big : Endian.little;
   }
-  public static void makeFloat$(ArrayBuffer self, List list)
+  public void endian(ArrayBuffer self, Endian v)
   {
-    FloatBuffer buf = BufferUtils.createFloatBuffer((int)list.size());
+    rawData.order(v == Endian.big ? ByteOrder.BIG_ENDIAN : ByteOrder.LITTLE_ENDIAN);
+  }
+
+  public NumType type(ArrayBuffer self)
+  {
+    java.nio.Buffer d = getValue();
+    if(d instanceof java.nio.ByteBuffer)
+    {
+      return NumType.tByte;
+    }
+    else if (d instanceof java.nio.FloatBuffer)
+    {
+      return NumType.tFloat;
+    }
+    else if (d instanceof java.nio.DoubleBuffer)
+    {
+      return NumType.tDouble;
+    }
+    else if(d instanceof java.nio.IntBuffer)
+    {
+      return NumType.tInt;
+    }
+    else if(d instanceof java.nio.ShortBuffer)
+    {
+      return NumType.tShort;
+    }
+    else if(d instanceof java.nio.CharBuffer)
+    {
+      return NumType.tChar;
+    }
+    else if(d instanceof java.nio.LongBuffer)
+    {
+      return NumType.tLong;
+    }
+    else
+    {
+      throw UnsupportedErr.make("unsupported type");
+    }
+  }
+
+  public void type(ArrayBuffer self, NumType v)
+  {
+    if (type(self) == v) return;
+
+    if(v == NumType.tByte)
+    {
+      bufferView = null;
+    }
+    else if (v == NumType.tFloat)
+    {
+      bufferView = rawData.asFloatBuffer();
+    }
+    else if (v == NumType.tDouble)
+    {
+      bufferView = rawData.asDoubleBuffer();
+    }
+    else if(v == NumType.tInt)
+    {
+      bufferView = rawData.asIntBuffer();
+    }
+    else if(v == NumType.tShort)
+    {
+      bufferView = rawData.asShortBuffer();
+    }
+    else if(v == NumType.tChar)
+    {
+      bufferView = rawData.asCharBuffer();
+    }
+    else if(v == NumType.tLong)
+    {
+      bufferView = rawData.asLongBuffer();
+    }
+    else
+    {
+      throw UnsupportedErr.make("unsupported type");
+    }
+  }
+
+  public long capacity(ArrayBuffer self) { return getValue().capacity(); }
+  public boolean isDirect(ArrayBuffer self) { return getValue().isDirect(); }
+
+  public ArrayBuffer flip(ArrayBuffer self) { getValue().flip(); return self; }
+  public long remaining(ArrayBuffer self) { return getValue().remaining(); }
+
+//////////////////////////////////////////////////////////////////////////
+// ctor
+//////////////////////////////////////////////////////////////////////////
+
+  public static ArrayBuffer allocateDirect(long size, NumType type)
+  {
+    ArrayBuffer ab = ArrayBuffer.make();
+    int capacity = (int)(size * type.size);
+    ab.peer.rawData = ByteBuffer.allocateDirect(capacity).order(ByteOrder.nativeOrder());
+    ab.type(type);
+    return ab;
+  }
+  public static ArrayBuffer allocateDirect(long size)
+  {
+    return allocateDirect(size, NumType.tByte);
+  }
+
+  public static ArrayBuffer allocate(long size, NumType type)
+  {
+    ArrayBuffer ab = ArrayBuffer.make();
+    int capacity = (int)(size * type.size);
+    ab.peer.rawData = ByteBuffer.allocate(capacity).order(ByteOrder.nativeOrder());
+    ab.type(type);
+    return ab;
+  }
+  public static ArrayBuffer allocate(long size)
+  {
+    return allocate(size, NumType.tByte);
+  }
+
+//////////////////////////////////////////////////////////////////////////
+// read/write
+//////////////////////////////////////////////////////////////////////////
+
+  public long getInt(ArrayBuffer self, long index)
+  {
+    IntBuffer buf = (IntBuffer)getValue();
+    return buf.get((int)index);
+  }
+  public ArrayBuffer set(ArrayBuffer self, long index, long v)
+  {
+    IntBuffer buf = (IntBuffer)getValue();
+    buf.put((int)index, (int)v);
+    return self;
+  }
+
+  public double getFloat(ArrayBuffer self, long index)
+  {
+    FloatBuffer buf = (FloatBuffer)getValue();
+    return buf.get((int)index);
+  }
+  public ArrayBuffer setFloat(ArrayBuffer self, long index, double v)
+  {
+    FloatBuffer buf = (FloatBuffer)getValue();
+    buf.put((int)index, (float)v);
+    return self;
+  }
+
+//////////////////////////////////////////////////////////////////////////
+// batch read/write
+//////////////////////////////////////////////////////////////////////////
+
+  public ArrayBuffer putFloat(ArrayBuffer self, List list)
+  {
+    FloatBuffer buf = (FloatBuffer)getValue();
     buf.put(toFloatArray(list));
-    buf.flip();
-    self.peer.data = buf;
+    return self;
   }
-
-  public static ArrayBuffer makeInt(List list)
+  public ArrayBuffer putInt(ArrayBuffer self, List list)
   {
-    ArrayBuffer buffer = ArrayBuffer.make();
-    makeInt$(buffer, list);
-    return buffer;
-  }
-  public static void makeInt$(ArrayBuffer self, List list)
-  {
-    IntBuffer buf = BufferUtils.createIntBuffer((int)list.size());
+    IntBuffer buf = (IntBuffer)getValue();
     buf.put(toIntArray(list));
-    buf.flip();
-    self.peer.data = buf;
+    return self;
   }
-
-  public static ArrayBuffer makeShort(List list)
+  public ArrayBuffer putShort(ArrayBuffer self, List list)
   {
-    ArrayBuffer buffer = ArrayBuffer.make();
-
-    ShortBuffer buf = BufferUtils.createShortBuffer((int)list.size());
+    ShortBuffer buf = (ShortBuffer)getValue();
     buf.put(toShortArray(list));
-    buf.flip();
-    buffer.peer.data = buf;
-
-    return buffer;
+    return self;
   }
 
 //////////////////////////////////////////////////////////////////////////
 // methods
 //////////////////////////////////////////////////////////////////////////
 
-  public java.nio.Buffer getValue() { return data; }
+  public java.nio.Buffer getValue()
+  {
+    if (bufferView != null) return bufferView;
+    return rawData;
+  }
+
+  public void getValue(java.nio.ByteBuffer buf)
+  {
+    bufferView = null;
+    rawData = buf;
+  }
 
 //////////////////////////////////////////////////////////////////////////
 // util
