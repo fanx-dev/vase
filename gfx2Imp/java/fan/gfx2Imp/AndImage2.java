87bd6b9453cb5834f1cc25a376cec1056cc7ed25
chunquedong <chunquedong@localhost>
2011-09-24 16:51:21 +0800

    file comment

--- gfx2Imp/java/fan/gfx2Imp/AndImage2.java
+++ gfx2Imp/java/fan/gfx2Imp/AndImage2.java
@@ -1,3 +1,10 @@
+//
+// Copyright (c) 2011, chunquedong
+// Licensed under the Academic Free License version 3.0
+//
+// History:
+//   2011-09-18  Jed Young  Creation
+//
 package fan.gfx2Imp;
 
 import java.io.OutputStream;
@@ -16,62 +23,62 @@ import fan.sys.UnsupportedErr;
 import fanx.interop.Interop;
 
 public class AndImage2  implements Image2{
-	
-	private Bitmap image;
-	public Bitmap getImage(){ return image; };
-	
-	public void setImage(Bitmap img){
-		image = img;
-	}
 
-	@Override
-	public Color getPixel(long x, long y) {
-		int argb = image.getPixel((int)x, (int)y);
-	    return fan.gfx.Color.make(argb, true);
-	}
+  private Bitmap image;
+  public Bitmap getImage(){ return image; };
 
-	@Override
-	public Graphics2 graphics() {
-		return new AndGraphics(new Canvas(image));
-	}
+  public void setImage(Bitmap img){
+    image = img;
+  }
 
-	@Override
-	public boolean isLoaded() {
-		return image != null;
-	}
+  @Override
+  public Color getPixel(long x, long y) {
+    int argb = image.getPixel((int)x, (int)y);
+      return fan.gfx.Color.make(argb, true);
+  }
 
-	@Override
-	public void save(OutStream out) {
-		save(out, MimeType.forExt("png"));
-	}
+  @Override
+  public Graphics2 graphics() {
+    return new AndGraphics(new Canvas(image));
+  }
 
-	@Override
-	public void save(OutStream out, MimeType format) {
-		OutputStream jout = Interop.toJava(out);
-	    
-	    CompressFormat swtFormat = Bitmap.CompressFormat.PNG;
-	    String subType = format.subType();
-	    if (subType.equals("png")) swtFormat = Bitmap.CompressFormat.PNG;
-	    else if (subType.equals("jpeg")) swtFormat = Bitmap.CompressFormat.JPEG;
-	    else if (subType.equals("jpg")) swtFormat = Bitmap.CompressFormat.JPEG;
-	    else throw UnsupportedErr.make("unsupported image type: "+subType);
-	    
-	    image.compress(swtFormat, 90, jout);
-	}
+  @Override
+  public boolean isLoaded() {
+    return image != null;
+  }
 
-	@Override
-	public void setPixel(long x, long y, Color value) {
-		image.setPixel((int)x, (int)y, (int)value.argb);
-	}
+  @Override
+  public void save(OutStream out) {
+    save(out, MimeType.forExt("png"));
+  }
 
-	@Override
-	public Size size() {
-		return Size.make(image.getWidth(), image.getHeight());
-	}
+  @Override
+  public void save(OutStream out, MimeType format) {
+    OutputStream jout = Interop.toJava(out);
 
-	@Override
-	public Image toImage() {
-		throw UnsupportedErr.make();
-	}
+      CompressFormat swtFormat = Bitmap.CompressFormat.PNG;
+      String subType = format.subType();
+      if (subType.equals("png")) swtFormat = Bitmap.CompressFormat.PNG;
+      else if (subType.equals("jpeg")) swtFormat = Bitmap.CompressFormat.JPEG;
+      else if (subType.equals("jpg")) swtFormat = Bitmap.CompressFormat.JPEG;
+      else throw UnsupportedErr.make("unsupported image type: "+subType);
+
+      image.compress(swtFormat, 90, jout);
+  }
+
+  @Override
+  public void setPixel(long x, long y, Color value) {
+    image.setPixel((int)x, (int)y, (int)value.argb);
+  }
+
+  @Override
+  public Size size() {
+    return Size.make(image.getWidth(), image.getHeight());
+  }
+
+  @Override
+  public Image toImage() {
+    throw UnsupportedErr.make();
+  }
 
 }
