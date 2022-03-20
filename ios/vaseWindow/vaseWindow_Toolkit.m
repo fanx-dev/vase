318a7774650670ddd08335c55519fb2e8da72e24
chunquedong <chunquedong@163.com>
2022-03-20 22:07:57 +0800

    init desityScale in Toolkit

--- ios/vaseWindow/vaseWindow_Toolkit.m
+++ ios/vaseWindow/vaseWindow_Toolkit.m
@@ -1,6 +1,9 @@
 #include "fni_ext.h"
 #include "pod_vaseWindow_native.h"
 
+#import <UIKit/UIKit.h>
+
+extern float desityScale;
 fr_Obj vaseWindow_Toolkit_instance = NULL;
 
 fr_Obj vaseWindow_Toolkit_cur(fr_Env env) {
@@ -8,6 +11,8 @@ fr_Obj vaseWindow_Toolkit_cur(fr_Env env) {
         fr_Obj obj = fr_newObjS(env, "vaseWindow", "NToolkit", "make", 0);
         fr_callOnObj(env, obj, "onInit", 0);
         vaseWindow_Toolkit_instance = fr_newGlobalRef(env, obj);
+        
+        desityScale = [[UIScreen mainScreen] scale];
     }
     return vaseWindow_Toolkit_instance;
 }
